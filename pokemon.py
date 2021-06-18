f6f6ebd38d50b73ef75fae38762f9b141720d06c
Steve <steve.e.rincon@gmail.com>
2021-06-17 23:33:47 -0400

    Pkm class and api

---
+++ pokemon.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+#created a class to store the static data of the pkm (i found this data in spreadsheet)
+class Pokemon:
+    def __init__(self, payload):
+        self.abilities = payload[0]
+        self.against_coefficients = {
+            "bug": float(payload[1]),
+            "dark": float(payload[2]),
+            "dragon": float(payload[3]),
+            "electric": float(payload[4]),
+            "fairy": float(payload[5]),
+            "fighting": float(payload[6]),
+            "fire": float(payload[7]),
+            "flying": float(payload[8]),
+            "ghost": float(payload[9]),
+            "grass": float(payload[10]),
+            "ground": float(payload[11]),
+            "ice": float(payload[12]),
+            "normal": float(payload[13]),
+            "poison": float(payload[14]),
+            "psychic": float(payload[15]),
+            "rock": float(payload[16]),
+            "steel": float(payload[17]),
+            "water": float(payload[18])
+        }
+        self.attack = float(payload[19])
+        self.base_egg_steps = float(payload[20])
+        self.base_happiness = float(payload[21])
+        self.base_total = float(payload[22])
+        self.capture_rate = float(payload[23])
+        self.classfication = payload[24]
+        self.defense = float(payload[25])
+        self.experience_growth = float(payload[26])
+        self.height_m = float(payload[27])
+        self.hp = float(payload[28])
+        self.name = payload[29]
+        self.percentage_male = float(payload[30])
+        self.pokedex_number = float(payload[31])
+        self.sp_attack = float(payload[32])
+        self.sp_defense = float(payload[33])
+        self.speed = float(payload[34])
+        self.type1 = payload[35]
+        self.type2 = payload[36]
+        self.weight_kg = float(payload[37])
+        self.generation = payload[38]
+        self.is_legendary = payload[39]
+
+
+    #I created methods to determin if one pokemon is stronger then other 
+    def IsStrongAgainst(self, pokemon_type):
+        # print("Strong: ", pokemon_type, self.against_coefficients[pokemon_type] if pokemon_type in self.against_coefficients else False)
+        #Found the coefficent of fighting based on pkm type and ensure that type is a number. Guardclause added since some data was emplty strings
+        if not pokemon_type in self.against_coefficients or isinstance(self.against_coefficients[pokemon_type], str):
+            return False
+        #if the coefficent is >1 then we are stronger then type check, this is true for the other type checkers
+        return self.against_coefficients[pokemon_type] > 1 or False
+    #abstracted version where you can use pkm class instead of type (using this one since I needed an avg)
+    def IsStrongerThan(self, pokemon):
+        Stronger = 0
+        AttributeCount = 0
+        if pokemon.type1 is not None and pokemon.type1 in self.against_coefficients:
+            AttributeCount += 1
+            Stronger += self.against_coefficients[pokemon.type1]
+        if pokemon.type2 is not None and pokemon.type2 in self.against_coefficients:
+            AttributeCount += 1
+            Stronger += self.against_coefficients[pokemon.type2]
+        return Stronger / AttributeCount > 1
+    #similar to stronger methods 
+    def IsWeakAgainst(self, pokemon_type):
+        # print("Weak: ", pokemon_type, self.against_coefficients[pokemon_type] if pokemon_type in self.against_coefficients else False)
+        if not pokemon_type in self.against_coefficients or isinstance(self.against_coefficients[pokemon_type], str):
+            return False
+        return self.against_coefficients[pokemon_type] < 1 or False
+    
+    def IsWeakerThan(self, pokemon):
+        Weaker = 0
+        AttributeCount = 0
+        if pokemon.type1 is not None and pokemon.type1 in self.against_coefficients:
+            AttributeCount += 1
+            Weaker += self.against_coefficients[pokemon.type1]
+        if pokemon.type2 is not None and pokemon.type2 in self.against_coefficients:
+            AttributeCount += 1
+            Weaker += self.against_coefficients[pokemon.type2]
+        return Weaker / AttributeCount < 1
+
+    #similar to stronger methods
+    def IsNeutralAgainst(self, pokemon_type):
+        # print("Neutral: ", pokemon_type, self.against_coefficients[pokemon_type] if pokemon_type in self.against_coefficients else False)
+        if not pokemon_type in self.against_coefficients or isinstance(self.against_coefficients[pokemon_type], str):
+            return False
+        return self.against_coefficients[pokemon_type] == 1.0 or self.against_coefficients[pokemon_type] == 1 or False
+
+    def IsNeutralTo(self, pokemon):
+        Neutral = 0
+        AttributeCount = 0
+        if pokemon.type1 is not None and pokemon.type1 in self.against_coefficients:
+            AttributeCount += 1
+            Neutral += self.against_coefficients[pokemon.type1]
+        if pokemon.type2 is not None and pokemon.type2 in self.against_coefficients:
+            AttributeCount += 1
+            Neutral += self.against_coefficients[pokemon.type2]
+        return Neutral / AttributeCount == 1
+
+    #returns all pkm types where this pkm is favored (used list comph for performance)
+    def StrongToTypes(self):
+        return [x for x in self.against_coefficients if self.against_coefficients[x] > 1]
+    
+    def WeakerToTypes(self):
+        return [x for x in self.against_coefficients if self.against_coefficients[x] < 1]
--- pokemon_api.py
+++ pokemon_api.py
@@ -1,9 +1,72 @@
+
 import csv
 import os
+import re 
+from pokemon import Pokemon
 
+# initialze static path vari 
 Root = os.getcwd()
-DatasetPath = os.path.join(Root, "datasets")
-PokemonDataset = os.path.join(DatasetPath, "pokemon.csv")
+PokemonDataset = os.path.join(Root, "datasets\\pokemon_stats\\pokemon.csv")
+
+# initialze bounds data to get an accurate idea of where pkm stat are relative
+LowerBounds = {
+    "hp": 1,
+    "attack": 5,
+    "defense": 5 ,
+    "sp_attack": 10,
+    "sp_defense": 20,
+    "speed": 5,
+}
+UpperBounds = {
+    "hp": 255,
+    "attack": 190,
+    "defense": 230,
+    "sp_attack": 194,
+    "sp_defense": 230,
+    "speed": 180,
+}
+
+# I got this from stacks overflow to convert rgb to hex because matplotlib uses hex values
+def rgb_to_hex(rgb_color):
+    rgb_color = re.search('\(.*\)', rgb_color).group(0).replace(' ', '').lstrip('(').rstrip(')')
+    [r, g, b] = [int(x) for x in rgb_color.split(',')]
+    # check if in range 0~255
+    assert 0 <= r <= 255
+    assert 0 <= g <= 255
+    assert 0 <= b <= 255
+ 
+    r = hex(r).lstrip('0x')
+    g = hex(g).lstrip('0x')
+    b = hex(b).lstrip('0x')
+    # re-write '7' to '07'
+    r = (2 - len(r)) * '0' + r
+    g = (2 - len(g)) * '0' + g
+    b = (2 - len(b)) * '0' + b
+ 
+    hex_color = '#' + r + g + b
+    return hex_color
+
+
+def FindPokemonByName(name, data = None):
+    if data is None:
+        data = ImportAndConstructPokemon()
+    name = name.capitalize()
+    return data[name] if name in data else None
+
+#delete later
+def FindAllPokemonByType(types, data = None):
+    if data is None:
+        data = ImportAndConstructPokemon()
+    types = [types] if not isinstance(types, list) else types
+ 
+    PokemonByType = []
+    for pokemon in data:
+        ThisPokemon = data[pokemon]
+        if ThisPokemon.type1 in types or ThisPokemon.type2 in types:
+            PokemonByType.append(ThisPokemon)
+
+    return PokemonByType
+
 
 def ImportAndConstructPokemon():
     with open(PokemonDataset) as ExcelFile:
@@ -11,12 +74,38 @@ def ImportAndConstructPokemon():
         Pokemen = {}
         try:
             for row in CsvReader:
-                ThisPokemon = row
-                if not row[29] in Pokemen:
-                    Pokemen[row[29]] = ThisPokemon
+                ThisPokemon = Pokemon(row)
+                if not ThisPokemon.name in Pokemen:
+                    Pokemen[ThisPokemon.name] = ThisPokemon
         except csv.Error as e:
             print(e)
 
         return Pokemen
-    
-print (ImportAndConstructPokemon())
+#initalize static pkm data for future use
+pokemonData = ImportAndConstructPokemon()
+def GetStatWeight(statType, amount):
+    if statType in UpperBounds:
+        return amount / UpperBounds[statType]
+
+def GetColorOfStatWeight(statType, amount, hex = False):
+    r, g = 1, 0
+    weight = GetStatWeight(statType, amount)
+    color = r - weight, g + weight, 0, 1
+    hexString = ""
+    if hex:
+        hexString += f"rgb({int(color[0]*255)},{int(color[1]*255)},{int(color[2]*255)})"
+        # hexString = rgb_to_hex(hexString)
+    return color if not hex else rgb_to_hex(hexString)
+
+def GetPictureForPokemon(pokemon):
+    files = [f for f in os.listdir("datasets\\pokemon_photos") if os.path.isfile(os.path.join("datasets\\pokemon_photos", f))]
+    if isinstance(pokemon, Pokemon):
+        for thisFile in files:
+            if pokemon.name.lower() in thisFile:
+                return os.path.join(os.getcwd(), "datasets\\pokemon_photos", thisFile)
+
+def GetPictureForType(pokemonType):
+    files = [f for f in os.listdir("datasets\\pokemon_types") if os.path.isfile(os.path.join("datasets\\pokemon_photos", f))]
+    for thisFile in files:
+        if pokemonType.lower() in thisFile:
+            return os.path.join(os.getcwd(), "datasets\\pokemon_types", thisFile)
